7184f29e82d093e35310c3478ca4042ed8153213
ImTheo-pdf <trejacks223@gmail.com>
2025-06-14 14:12:40 -0400

    Finished TAsk 4

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //PSEUDO CODE
+        //Create Scanner
+        //int age
+        //String trash
+        //Output "Enter your age: "
+        //Input age
+        //if input is an int
+        //  if age >= 21
+        //      Output "You get a wrist band"
+        //Else
+        // Output "Invalid Input"
+        //CODE
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner in = new Scanner(System.in);
+        int age = 0;
+        String trash = "";
+
+        System.out.print("Enter your age: ");
+
+        if(in.hasNextInt()){
+            age = in.nextInt();
+            in.nextLine();
+
+            if (age >= 21) {
+                System.out.println("You get a wrist band.");
+            }
         }
+        else{
+            trash = in.nextLine();
+            System.out.println("Invalid input: " + trash);
+            System.out.println("Enter your age");
+        }
+
     }
 }
